4bcef4ed1233f566af7aa3d6a7e5b9a24255add2
LiviaMedeiros <livia@cirno.name>
2023-08-17 02:19:24 +0800

    add `poteto-cat`

---
+++ bin/poteto-cat.mjs
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import poteto from '../index.mjs?persistCwd=true';
+
+const printResponse = async ({ ok, body }) => {
+  for await (const $ of body)
+    process[ok ? 'stdout' : 'stderr'].write($);
+}
+
+const [,,...urls] = process.argv;
+
+// chunks from different files may come in arbitrary order
+// low memory footprint
+// READ method will be used even for remote addresses
+const catConcurrent = urls =>
+  Promise.all(urls.map(url =>
+    poteto(url, { method: 'READ' }).then(printResponse)
+  ));
+
+// everything comes in order
+// each local file loaded in memory
+// GET method for all
+const catOrder = async urls => {
+  for (const url of urls)
+    await poteto(url).then(printResponse);
+    //await printResponse(await poteto(url));
+}
+
+await catOrder(urls);
+//await catConcurrent(urls);
